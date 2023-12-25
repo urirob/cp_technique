c3697fe08fd2e29452d76bae243e0fe20041ee8a
Tarundeep Singh <91110790+urirob@users.noreply.github.com>
2023-12-25 14:56:31 +0530

    Create flip_msb_and_lsb.cpp

---
+++ bit_manipulation/flip_msb_and_lsb.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int n;
+	cin>>n;
+	//flip msb and lsb and print the decimal equivalent of that number
+	
+	n=n^1;
+	
+	int pos=0;
+	int n1=n;
+	while(n1!=0){
+	    n1=n1/2;
+	    pos++;
+	}
+	
+	pos--;          //because of 0 based indexing in bitwise representation
+	
+	n=n^(1>>pos);
+	
+	
+	cout<<n<<"\n";
+	
+	
+
+}
